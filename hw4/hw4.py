8d1f8709782c0f644952006dbc3439e54c24266a
nieves <930112ab@gmail.com>
2025-03-28 23:34:09 +0800

    add: output images

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -16,6 +16,10 @@ rotated_nearest = cv2.warpAffine(image, M, (size, size), flags=cv2.INTER_NEAREST
 # bilinear interpolation
 rotated_bilinear = cv2.warpAffine(image, M, (size, size), flags=cv2.INTER_LINEAR)
 
+cv2.imwrite("original.png", image)                # 儲存原始影像
+cv2.imwrite("rotated_nearest.png", rotated_nearest)  # 儲存最近鄰插值結果
+cv2.imwrite("rotated_bilinear.png", rotated_bilinear)  # 儲存雙線性插值結果
+
 # 顯示結果
 fig, axs = plt.subplots(1, 3, figsize=(12, 4))
 axs[0].imshow(image, cmap='gray')
